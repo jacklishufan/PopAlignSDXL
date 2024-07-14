8846b4eebd0809998ba1c8733fae730e0aba995b
Shufan Li <jacklishufan@cs.ucla.edu>
2024-07-14 13:18:03 -0700

    fix data gen bug

--- 2_generate_images.sh
+++ 2_generate_images.sh
@@ -1,13 +1,13 @@
 export SDXL="stabilityai/stable-diffusion-xl-base-1.0"
-export OUTPUT_DIR_DIVERSE="your path here"
-export OUTPUT_DIR_BASE="your path here"
-
-ANNO_DIVERSE="data/training_prompts_aug.csv"
-ANNO_BASE="data/training_prompts.csv"
-
-bash scripts/eval_run.sh 8  $SDXL $OUTPUT_DIR_DIVERSE 10 $ANNO_DIVERSE 
-bash scripts/eval_run.sh 8  $SDXL $OUTPUT_DIR_BASE 10 $ANNO_BASE 
+export OUTPUT_DIR_DIVERSE="img/sdxl_diverse"
+export OUTPUT_DIR_BASE="img/sdxl_base"
+ANNO_DIVERSE="data/eval_prompts_aug.csv"
+ANNO_BASE="data/eval_prompts.csv"
+rm -rf $OUTPUT_DIR_DIVERSE
+rm -rf $OUTPUT_DIR_BASE
+bash scripts/eval_run_sd15.sh 8  $SDXL $OUTPUT_DIR_DIVERSE 10 $ANNO_DIVERSE 
+bash scripts/eval_run_sd15.sh 8  $SDXL $OUTPUT_DIR_BASE 100 $ANNO_BASE 
 
 # run classifier
-CUDA_VISIBLE_DEVICES=0 python eval/classifier.py -p $ANNO_DIVERSE
-CUDA_VISIBLE_DEVICES=0 python eval/classifier.py -p $ANNO_BASE
+CUDA_VISIBLE_DEVICES=0 python eval/classifier.py -p $OUTPUT_DIR_DIVERSE & 
+CUDA_VISIBLE_DEVICES=1 python eval/classifier.py -p $OUTPUT_DIR_BASE
--- 3_generate_preferences.py
+++ 3_generate_preferences.py
@@ -3,10 +3,11 @@ import numpy as np
 from itertools import product
 races = ['white','asian','black','latino hispanic',"indian",'middle eastern']
 genders = ['male','female']
+OUTPUT_DIR_DIVERSE="img/sdxl_diverse"
+OUTPUT_DIR_BASE="img/sdxl_base"
 
-
-records_ref = pd.read_csv('<your folder here>/race_gender.csv')
-records_good = pd.read_csv('<your folder here>/race_gender.csv')
+records_ref = pd.read_csv(f'{OUTPUT_DIR_BASE}/race_gender.csv')
+records_good = pd.read_csv(f'{OUTPUT_DIR_DIVERSE}/race_gender.csv')
 records_good['Raw Prompt']=records_good['prompt']
 for race,gender in product(races,genders):
         prompts_aug = f'a realistic photo of a {race} {gender} '
@@ -29,25 +30,25 @@ for _ in range(int(100*10*2*5*2)):
     raw_prompt = np.random.choice(list(cap_dict.keys()))
     if not cap_dict[raw_prompt] or not raw_dict[raw_prompt]:
         continue
-    a = np.random.choice(cap_dict[raw_prompt],size=4,replace=True)
-    b = np.random.choice(raw_dict[raw_prompt],size=4,replace=True)
-    race_a = np.unique([x['Race'] for x in a])
-    race_b = np.unique([x['Race'] for x in b])
-    gender_a = np.unique([x['Gender'] for x in a])
-    gender_b = np.unique([x['Gender'] for x in b])
+    a = np.random.choice(cap_dict[raw_prompt],size=5,replace=True)
+    b = np.random.choice(raw_dict[raw_prompt],size=5,replace=True)
+    race_a = np.unique([x['race'] for x in a])
+    race_b = np.unique([x['race'] for x in b])
+    gender_a = np.unique([x['gender'] for x in a])
+    gender_b = np.unique([x['gender'] for x in b])
     # sanity check
-    better1 = (len(race_a) >= len(race_b)) and (len(race_a) >= len(race_b))
+    better1 = (len(race_a) >= len(race_b)) or (len(gender_a) >= len(gender_b))
     if not better1:
         a,b = b,a
+    # breakpoint()
     for idx in range(len(a)):
         new_data = dict(
             jpg_0 = a[idx]['file_path'], 
             jpg_1 = b[idx]['file_path'],
-            caption=a[idx]['Raw Prompt'],
+            caption=a[idx]['Raw Prompt'] if 'Raw Prompt' in a[idx] else b[idx]['Raw Prompt'],
             label=0, 
         )
-        if a[idx]['gender'] == 'Woman':
-            all_new_data.append(new_data)
+        all_new_data.append(new_data)
 df = pd.DataFrame(all_new_data)
 print(len(np.unique(df.caption)))
 df.to_csv('data/annotation_train.csv',index=False)
