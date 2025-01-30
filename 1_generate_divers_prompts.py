500358adfa2d93259d0b4754a51240059e128e4c
Shufan Li <jacklishufan@cs.ucla.edu>
2025-01-30 15:08:14 -0800

    update data for additional evals, add age and sexual orientation data

--- 1_generate_divers_prompts.py
+++ 1_generate_divers_prompts.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from itertools import product
+
+## GENDER
 races = ['white','asian','black','latino hispanic',"indian",'middle eastern']
 genders = ['male','female']
 input_csv = 'data/training_prompts.csv'
@@ -11,5 +13,23 @@ for p in prompts:
         new_prompts.append(prompts_aug)
 df = pd.DataFrame(new_prompts)
 df.columns = ['Prompt']
-df.to_csv(input_csv.replace('.csv','_aug.csv'),index=False)
+df.to_csv(input_csv.replace('.csv','_aug_racegender.csv'),index=False)
+# breakpoint()
+
+## AGE
+import pandas as pd
+from itertools import product
+# races = ['white','asian','black','latino hispanic',"indian",'middle eastern']
+# genders = ['male','female']
+ages = ['young','mid-aged','very old']
+input_csv = 'data/training_prompts.csv'
+prompts = pd.read_csv(input_csv).Prompt
+new_prompts = []
+for p in prompts[:100]:
+    for age in ages:
+        prompts_aug = f'a realistic photo of a {age} '+ p
+        new_prompts.append(prompts_aug)
+df = pd.DataFrame(new_prompts)
+df.columns = ['Prompt']
+df.to_csv(input_csv.replace('.csv','_aug_age.csv'),index=False)
 # breakpoint()
--- 2_generate_images.sh
+++ 2_generate_images.sh
@@ -1,13 +1,32 @@
 export SDXL="stabilityai/stable-diffusion-xl-base-1.0"
-export OUTPUT_DIR_DIVERSE="img/sdxl_diverse"
-export OUTPUT_DIR_BASE="img/sdxl_base"
-ANNO_DIVERSE="data/eval_prompts_aug.csv"
-ANNO_BASE="data/eval_prompts.csv"
-rm -rf $OUTPUT_DIR_DIVERSE
-rm -rf $OUTPUT_DIR_BASE
-bash scripts/eval_run_sd15.sh 8  $SDXL $OUTPUT_DIR_DIVERSE 10 $ANNO_DIVERSE 
-bash scripts/eval_run_sd15.sh 8  $SDXL $OUTPUT_DIR_BASE 100 $ANNO_BASE 
+export OUTPUT_DIR_DIVERSE="img/sdxl_diverse_racegender"
+export OUTPUT_DIR_AGE="img/sdxl_diverse_age"
+export OUTPUT_DIR_SEXUAL_ORIENTATION_DIVERSE="img/sdxl_diverse_couple"
 
-# run classifier
-CUDA_VISIBLE_DEVICES=0 python eval/classifier.py -p $OUTPUT_DIR_DIVERSE & 
-CUDA_VISIBLE_DEVICES=1 python eval/classifier.py -p $OUTPUT_DIR_BASE
+export OUTPUT_DIR_BASE="img/sdxl_base"
+export OUTPUT_DIR_SEXUAL_ORIENTATION_BASE="img/sdxl_base_couple"
+
+
+
+ANNO_DIVERSE_SEXUAL_ORIENTATION="data/training_prompts_aug_racegender.csv"
+ANNO_BASE_SEXUAL_ORIENTATION="data/training_prompts_aug_racegender.csv"
+ANNO_DIVERSE_RACEGENDER="data/training_prompts_aug_racegender.csv"
+ANNO_DIVERSE_AGE="data/training_prompts_aug_age.csv"
+ANNO_BASE="data/training_prompts.csv"
+
+
+bash scripts/eval_run.sh 8  $SDXL $OUTPUT_DIR_DIVERSE 10 $ANNO_DIVERSE_RACEGENDER 
+bash scripts/eval_run.sh 8  $SDXL $OUTPUT_DIR_AGE 100 $ANNO_DIVERSE_AGE 
+bash scripts/eval_run.sh 8  $SDXL $OUTPUT_DIR_SEXUAL_ORIENTATION_DIVERSE 100 $ANNO_DIVERSE_SEXUAL_ORIENTATION 
+
+
+bash scripts/eval_run.sh 8  $SDXL $OUTPUT_DIR_BASE 100 $ANNO_BASE 
+bash scripts/eval_run.sh 8  $SDXL $OUTPUT_DIR_SEXUAL_ORIENTATION_BASE 100 $ANNO_BASE_SEXUAL_ORIENTATION 
+
+
+# run classifier in parallel
+CUDA_VISIBLE_DEVICES=0 python eval/classifier.py -p $OUTPUT_DIR_DIVERSE 
+CUDA_VISIBLE_DEVICES=0 python eval/classifier.py -p $OUTPUT_DIR_AGE 
+CUDA_VISIBLE_DEVICES=0 python eval/classifier.py -p $OUTPUT_DIR_SEXUAL_ORIENTATION_DIVERSE
+CUDA_VISIBLE_DEVICES=0 python eval/classifier.py -p $OUTPUT_DIR_BASE
+CUDA_VISIBLE_DEVICES=0 python eval/classifier.py -p $OUTPUT_DIR_SEXUAL_ORIENTATION_BASE
